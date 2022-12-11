fd495f5dbf7732a1118e569a59a5a201bb9d65a9
Pxrsona <64496306+OmarObando@users.noreply.github.com>
2022-12-11 17:00:20 -0600

    Restrict Copy Paste

--- WPFLayer/GameMenu.xaml.cs
+++ WPFLayer/GameMenu.xaml.cs
@@ -115,5 +115,16 @@ namespace WPFLayer
             BanUsersPage banUsersPage = new BanUsersPage();
             this.NavigationService.Navigate(banUsersPage);
         }
+
+        private void textBox_PreviewExecuted(object sender, ExecutedRoutedEventArgs e)
+        {
+            if (e.Command == ApplicationCommands.Copy ||
+                e.Command == ApplicationCommands.Cut ||
+                e.Command == ApplicationCommands.Paste)
+            {
+                e.Handled = true;
+            }
+        }
+
     }
 }
