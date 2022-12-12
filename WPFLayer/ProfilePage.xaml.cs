6bf7ffaaaa62ca3e176ee986c424919d52dc19a1
Javier Domínguez-Chavi <josejdc070@gmail.com>
2022-12-12 10:18:53 -0600

    Fixed standard issues

--- WPFLayer/ProfilePage.xaml.cs
+++ WPFLayer/ProfilePage.xaml.cs
@@ -24,12 +24,12 @@ namespace WPFLayer
     /// </summary>
     public partial class ProfilePage : Page, ServicesImplementation.IUsersManagerCallback
     {
-        public ServicesImplementation.Players playerLogin { get; set; }
+        public ServicesImplementation.Players PlayerLogin { get; set; }
         public ProfilePage(ServicesImplementation.Players player)
         {
-            this.playerLogin = player;
+            this.PlayerLogin = player;
             InitializeComponent();
-            playerLogin = GetPlayerProfileInformation();
+            PlayerLogin = GetPlayerProfileInformation();
             ShowProfileView();
         }
 
@@ -38,32 +38,32 @@ namespace WPFLayer
             InstanceContext context = new InstanceContext(this);
             ServicesImplementation.UsersManagerClient client = new ServicesImplementation.UsersManagerClient(context);
             ServicesImplementation.Players player = new ServicesImplementation.Players();
-            player = playerLogin;
+            player = PlayerLogin;
             player = client.GetPlayerInformation(player);
             return player;
         }
 
         public void ShowProfileView()
         {
-            this.TextBox_IdPlayer.Text = "ID:  " + playerLogin.idPlayer.ToString();
-            this.TextBox_PlayerName.Text = playerLogin.playerName;
-            this.TextBlock_PlayerLevelNumber.Text = playerLogin.playerLevel.ToString();
-            this.TextBlock_PlayerProfileDescription.Text = playerLogin.playerDescription;
-            this.TextBlock_GamesPlayedNumber.Text = playerLogin.gamesPlayed.ToString();
-            this.TextBlock_GamesWonNumber.Text = playerLogin.gamesWon.ToString();
-            Bitmap bitmapProfileImage = (Bitmap)Properties.ResourcesProfileImages.ResourceManager.GetObject(playerLogin.ProfileImage.profileImageName);
+            this.TextBox_IdPlayer.Text = "ID:  " + PlayerLogin.idPlayer.ToString();
+            this.TextBox_PlayerName.Text = PlayerLogin.playerName;
+            this.TextBlock_PlayerLevelNumber.Text = PlayerLogin.playerLevel.ToString();
+            this.TextBlock_PlayerProfileDescription.Text = PlayerLogin.playerDescription;
+            this.TextBlock_GamesPlayedNumber.Text = PlayerLogin.gamesPlayed.ToString();
+            this.TextBlock_GamesWonNumber.Text = PlayerLogin.gamesWon.ToString();
+            Bitmap bitmapProfileImage = (Bitmap)Properties.ResourcesProfileImages.ResourceManager.GetObject(PlayerLogin.ProfileImage.profileImageName);
             BitmapSource bitmapProfileImageSource = Imaging.CreateBitmapSourceFromHBitmap(bitmapProfileImage.GetHbitmap(), IntPtr.Zero, Int32Rect.Empty, BitmapSizeOptions.FromEmptyOptions());
             this.Image_PlayerProfile.Source = bitmapProfileImageSource;
             this.Image_PlayerProfile.Stretch = Stretch.Fill;
-            Bitmap bitmapRankImage = (Bitmap)Properties.ResourcesPlayerRankImages.ResourceManager.GetObject(playerLogin.Rank.rankName);
+            Bitmap bitmapRankImage = (Bitmap)Properties.ResourcesPlayerRankImages.ResourceManager.GetObject(PlayerLogin.Rank.rankName);
             BitmapSource bitmapRankImageSource = Imaging.CreateBitmapSourceFromHBitmap(bitmapRankImage.GetHbitmap(), IntPtr.Zero, Int32Rect.Empty, BitmapSizeOptions.FromEmptyOptions());
             this.Image_PlayerRank.Source = bitmapRankImageSource;
-            this.Label_PlayerRank.Content = playerLogin.Rank.rankName;
+            this.Label_PlayerRank.Content = PlayerLogin.Rank.rankName;
         }
 
         private void Button_EditProfile_Click(object sender, RoutedEventArgs e)
         {
-            UpdateProfilePage updateProfilePage = new UpdateProfilePage(playerLogin);
+            UpdateProfilePage updateProfilePage = new UpdateProfilePage(PlayerLogin);
             this.NavigationService.Navigate(updateProfilePage);
         }
 
